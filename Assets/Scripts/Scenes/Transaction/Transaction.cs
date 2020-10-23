75bb3ab16b32e592dda4eb5cdc837e55f83d4d57
ismailonur <ismailonur15@gmail.com>
2020-10-23 10:16:09 +0300

    Transaction sahnesi oluşturuldu

---
+++ Assets/Scripts/Scenes/Transaction/Transaction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Transaction : MonoBehaviour
+{
+    private DBManager DB;
+
+    private UserData user;
+    private RoomData room;
+
+    public Text playerAReadyText;
+    public Text playerBReadyText;
+
+    public Text noticeText;
+
+    public GameObject inviteObject;
+
+    private void Awake()
+    {
+        DB = DBManager.Instance;
+
+        user = UserData.Instance;
+        room = RoomData.Instance;
+    }
+
+    void Start()
+    {
+        user.gameState = GameState.Transaction;
+        DB.SetReady();
+    }
+
+    void Update()
+    {
+        
+    }
+}
